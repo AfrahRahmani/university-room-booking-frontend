a2d7da6f180fecba357d416be82e07bba81d1db5
AfrahRahmani <afrah.s.alrahmani@gmail.com>
2025-12-07 15:34:09 +0300

    fixing duplicate with api

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -4,8 +4,7 @@
 // Why? So we can change it in ONE place instead of searching the whole project.
 // Production API URL
 // Local backend URL during development
-const BASE_URL = import.meta.env.VITE_API_URL || 
-"http://localhost:5000/api";
+const BASE_URL = "https://university-room-booking-backend.onrender.com/api";
 
 // Export it so we can import it anywhere in the frontend.
 export default BASE_URL;
